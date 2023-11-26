b05eb285ed9dea790a8b6897cbca46b9069554b2
abe <abel.rogers@gmail.com>
2023-11-26 17:10:50 +0000

    cookie consent component

---
+++ src/cookieConsent/CookieConsentComponent.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import CookieConsent from 'react-cookie-consent';
+
+type CookieConsentComponentProps = {
+  onAccept: () => void;
+  showNotificationConsent: boolean;
+};
+
+const CookieConsentComponent: React.FC<CookieConsentComponentProps> = ({
+  onAccept,
+  showNotificationConsent,
+}) => {
+  const handleDecline = () => {
+    window.location.href = 'https://www.google.com';
+  };
+
+  return (
+    <>
+      <CookieConsent
+        location="bottom"
+        buttonText="Aceitar todos os cookies"
+        enableDeclineButton
+        declineButtonText="Recusar"
+        onAccept={onAccept}
+        onDecline={handleDecline}
+        style={{ background: '#ffffff' }}
+        cookieName="userConsentCookie"
+        containerClasses="px-3 mx-auto p-4 text-left text-sm border-t-3 border-solid border-gray"
+        buttonClasses="justify-center items-center gap-2 py-3 px-5 w-60 h-11 rounded-[0.3125rem] bg-[#ffcc23] Sans 3'] text-[#13161a] font-['Source font-semibold leading-[150%] border-t-1 border-gray"
+        declineButtonClasses="Sans 3'] text-[#013a67] font-['Source font-semibold leading-[150%]"
+        expires={150}
+      >
+        <h3 className="Condensed'] font-['Roboto text-lg font-bold leading-[145%] text-[#13161a]">
+          Nós usamos cookies na entrega de nossos serviços
+        </h3>
+        <p className="Sans 3'] font-['Source leading-[145%] text-[#2c2f33]">
+          Para continuar a utilizar esse site, você consente no uso de cookies
+          em acordo com nossa política de cookies.
+        </p>
+      </CookieConsent>
+      {showNotificationConsent && (
+        <CookieConsent
+          location="bottom"
+          buttonText="Ativar notificações"
+          enableDeclineButton
+          declineButtonText="Agora não"
+          onAccept={() => console.log('Notifications accepted')}
+          onDecline={() => console.log('Notifications declined')}
+          style={{ background: '#ffffff' }}
+          cookieName="notificationConsentCookie"
+          containerClasses="px-3 mx-auto p-4 text-left text-sm border-t-3 border-solid border-gray"
+          buttonClasses="justify-center items-center gap-2 py-3 px-5 w-60 h-11 rounded-[0.3125rem] bg-[#ffcc23] Sans 3'] text-[#13161a] font-['Source font-semibold leading-[150%] border-t-1 border-gray"
+          declineButtonClasses="Sans 3'] text-[#013a67] font-['Source font-semibold leading-[150%]"
+          expires={150}
+        >
+          <h3 className="Condensed'] font-['Roboto text-lg font-bold leading-[145%] text-[#13161a]">
+            Ative Notificações Push
+          </h3>
+          <p className="Sans 3'] font-['Source leading-[145%] text-[#2c2f33]">
+            Não perca mais nada! Fique atualizado com as últimas notícias,
+            promoções exclusivas e atualizações importantes.
+          </p>
+        </CookieConsent>
+      )}
+    </>
+  );
+};
+
+export default CookieConsentComponent;
--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+import CookieConsentComponent from '../cookieConsent/CookieConsentComponent';
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
@@ -6,15 +9,30 @@ import { Footer } from './Footer';
 import Form from './Form';
 import { Hero } from './Hero';
 
-const Base = () => (
-  <div className="text-gray-600 antialiased">
-    <Meta title={AppConfig.title} description={AppConfig.description} />
-    <Hero />
-    <ComparisonCards />
-    <Form />
-    <Banner />
-    <Footer />
-  </div>
-);
+const Base: React.FC = () => {
+  // Define state and handler inside the functional component
+  const [showNotificationConsent, setShowNotificationConsent] = useState(false);
+
+  const handleCookieConsentAccept = () => {
+    setTimeout(() => {
+      setShowNotificationConsent(true);
+    }, 40000); // 40 second delay
+  };
+
+  return (
+    <div className="text-gray-600 antialiased">
+      <Meta title={AppConfig.title} description={AppConfig.description} />
+      <Hero />
+      <ComparisonCards />
+      <Form />
+      <Banner />
+      <Footer />
+      <CookieConsentComponent
+        onAccept={handleCookieConsentAccept}
+        showNotificationConsent={showNotificationConsent}
+      />
+    </div>
+  );
+};
 
 export { Base };
